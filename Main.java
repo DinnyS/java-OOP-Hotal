87f7ad477714000ac10d4f653d98e9e7e1c71d61
DinnyS <crysisdin123@gmail.com>
2023-04-05 16:23:46 +0700

    Mainnnnn

--- Main.java
+++ Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(" ----- Welcome to Mackky Hotel 5 Star ----- ");
+        System.out.println();
+        System.out.println();
+
+        System.out.println(" == Select == ");
+        System.out.println(" 1. Info Room ");
+        System.out.println(" 2. Booking");
+
+    }
 }
