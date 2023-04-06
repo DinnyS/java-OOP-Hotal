6e3035cf7ee9f072ad8a97e68f66883c512255ea
Joon <yada.2546.kks@gmail.com>
2023-04-06 22:00:18 +0700

    Pluto

--- Main.java
+++ Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println(" == Select == ");
         System.out.println(" 1. Info Room ");
         System.out.println(" 2. Booking");
-        System.out.println("hello Jupiter");
+        System.out.println("hello Pluto");
 
 
     }
