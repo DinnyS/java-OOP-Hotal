88065c5e0138fea414f111106135940c8900793c
frKnarf <119650431+frKnarf@users.noreply.github.com>
2023-06-16 15:02:10 +0700

    F

--- Booking/AvailableHotel.java
+++ Booking/AvailableHotel.java
@@ -1,5 +1,27 @@
 package Booking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AvailableHotel extends Bill {
-    
+
+    private static List<String> allOfHotel = new ArrayList<>(); // arraylist วัน / เดือน / ปี / จำนวนห้องหลักสิบ / จำนวนห้องหลักหน่วย / ชนิดห้อง
+
+    public AvailableHotel(){}
+
+    public AvailableHotel(int day, int month, int year, int available, int type, String checkInDate){
+        
+        String[] parts = checkInDate.split("/");
+            if (parts.length == 4) {
+                day = Integer.parseInt(parts[0]);
+                month = Integer.parseInt(parts[1]);
+                year = Integer.parseInt(parts[2]);
+                type = Integer.parseInt(parts[3]);
+                System.out.println("");
+                System.out.println("Check In : " + day +"/"+ month +"/"+ year + "/" + type);
+            }
+
+            int count = 0;
+            int roomHotel = 0;
+    }
 }
